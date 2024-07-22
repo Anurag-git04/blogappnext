8efb5629ab71e9a30db66d0c8502b9a7ee5adf67
Anurag-git04 <shawanurag18@gmail.com>
2024-07-22 23:57:37 +0530

    first

---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between'>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
